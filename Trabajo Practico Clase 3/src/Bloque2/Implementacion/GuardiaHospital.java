fb8e31484ae196ac5cbcd71a5fe75e53177971df
IvanPlaza44 <plazaivan44@gmail.com>
2026-03-23 20:50:19 -0300

    Bloque 2, hasta ejercicio 3

---
+++ Trabajo Practico Clase 3/src/Bloque2/Implementacion/GuardiaHospital.java
@@ -0,0 +1,71 @@
+package Bloque2.Implementacion;
+
+import Bloque2.Interfaces.ColasPrioridad;
+
+public class GuardiaHospital implements ColasPrioridad {
+
+    private int[] pacientes;
+    private int[] prioridades;
+    private int cantidad;
+
+    @Override
+    public void InicializarCola() {
+        pacientes = new int[100];
+        prioridades = new int[100];
+        cantidad = 0;
+    }
+
+    @Override
+    public void AcolarPrioridad(int paciente, int prioridad) {
+        pacientes[cantidad] = paciente;
+        prioridades[cantidad] = prioridad;
+        cantidad++;
+    }
+
+    @Override
+    public void Desacolar() {
+        int pos = 0;
+
+        for (int i = 1; i < cantidad; i++) {
+            if (prioridades[i] > prioridades[pos]) {
+                pos = i;
+            }
+        }
+
+        pacientes[pos] = pacientes[cantidad - 1];
+        prioridades[pos] = prioridades[cantidad - 1];
+
+        cantidad--;
+    }
+
+    @Override
+    public int Primero() {
+        int pos = 0;
+
+        for (int i = 1; i < cantidad; i++) {
+            if (prioridades[i] > prioridades[pos]) {
+                pos = i;
+            }
+        }
+
+        return pacientes[pos];
+    }
+
+    @Override
+    public int Prioridad() {
+        int pos = 0;
+
+        for (int i = 1; i < cantidad; i++) {
+            if (prioridades[i] > prioridades[pos]) {
+                pos = i;
+            }
+        }
+
+        return prioridades[pos];
+    }
+
+    @Override
+    public boolean ColaVacia() {
+        return cantidad == 0;
+    }
+}
---
+++ Trabajo Practico Clase 3/src/Bloque2/Interfaces/Colas.java
@@ -0,0 +1,11 @@
+package Bloque2.Interfaces;
+
+public interface Colas {
+
+    void InicializarCola();
+    void Acolar(int x);
+    void Desacolar();
+    int Primero();
+    boolean ColaVacia();
+
+}
