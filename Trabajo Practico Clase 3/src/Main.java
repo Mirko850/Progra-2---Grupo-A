f7754a2e755f032684ac2a7ea6fbbb5fb37f542c
IvanPlaza44 <plazaivan44@gmail.com>
2026-03-23 20:56:26 -0300

    Main agregado

---
+++ Trabajo Practico Clase 3/src/Main.java
@@ -0,0 +1,69 @@
+import Bloque2.Implementacion.FilaCajero;
+import Bloque2.Implementacion.GuardiaHospital;
+import Bloque2.Implementacion.ImpresoraLab;
+import Bloque2.Interfaces.Colas;
+import Bloque2.Interfaces.ColasPrioridad;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // ---------- Pruebas Fila Cajero
+        System.out.println("Pruebas Fila de Cajero");
+        ColasPrioridad fila = new FilaCajero();
+        fila.InicializarCola();
+
+        // cliente normal
+        fila.AcolarPrioridad(1, 1);
+
+        // embarazada
+        fila.AcolarPrioridad(2, 10);
+
+        // jubilado
+        fila.AcolarPrioridad(3, 5);
+
+        System.out.println("Es turno del cliente Nro: " + fila.Primero());
+
+        // ------------ Prueba Impresora Lab
+        System.out.println("Pruebas Impresora Lab");
+
+        Colas impresora = new ImpresoraLab();
+        impresora.InicializarCola();
+
+        // llegan 5 archivos
+        impresora.Acolar(1);
+        impresora.Acolar(2);
+        impresora.Acolar(3);
+        impresora.Acolar(4);
+        impresora.Acolar(5);
+
+        // orden de impresión
+        while (!impresora.ColaVacia()) {
+            System.out.println("Imprimiendo archivo: " + impresora.Primero());
+            impresora.Desacolar();
+        }
+
+        // --------- Pruebas Guardia Hospital de clinicas
+        System.out.println("Pruebas Guardia Hospital");
+        ColasPrioridad triage = new GuardiaHospital();
+        triage.InicializarCola();
+
+        // llegan en cualquier orden
+        triage.AcolarPrioridad(1, 1);   // raspón
+        triage.AcolarPrioridad(2, 100); // bobazo
+        triage.AcolarPrioridad(3, 20);  // corte
+        triage.AcolarPrioridad(4, 70);  // fractura
+        triage.AcolarPrioridad(5, 10);  // muela
+
+        // atención por prioridad
+        while (!triage.ColaVacia()) {
+            System.out.println(
+                    "Paciente: " + triage.Primero() +
+                            " | Prioridad: " + triage.Prioridad()
+            );
+            triage.Desacolar();
+        }
+
+
+    }
+}
