b55486a8605b515a6f6e835116fc51ca2444b204
Mirko <mirko.palma850@gmail.com>
2026-03-30 16:59:18 -0300

    Create ConjuntoTDA.java

---
+++ Trabajo Practico Clase 4/src/Bloque3/ImplementacionConjuntos/ConjuntoTDA.java
@@ -0,0 +1,8 @@
+public interface ConjuntoTDA {
+    void InicializarConjunto();
+    void Agregar(int x);
+    void Sacar(int x);
+    boolean Pertenece(int x);
+    int Elegir();
+    boolean ConjuntoVacio();
+}
